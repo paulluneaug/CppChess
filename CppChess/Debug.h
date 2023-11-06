ee5cd198c5e751f513749c2666487eb787b955b2
p.luneau <paul.luneau@mail.com>
2023-11-06 16:45:20 +0100

    Debug class, empty for now

---
+++ CppChess/Debug.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+namespace CppChess 
+{
+	static class Debug
+	{
+	public:
+		__forceinline static void Log(std::string message)
+		{
+			std::cout << message;
+		}
+
+		__forceinline static void LogWarning(std::string warning)
+		{
+			std::cout << warning;
+		}
+
+		__forceinline static void LogError(std::string error)
+		{
+			std::cout << error;
+		}
+	};
+}
+
